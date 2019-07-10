bb234773e349b251501e5d480cddcf715853d68e
unknown <ruizkelani@gmail.com>
2019-07-10 09:42:19 -0700

    this is my first commit

---
+++ home.js
@@ -0,0 +1,29 @@
+function alert_name () {
+  //create name variable
+  var name = "Kelani";
+  alert("My name is" + name)
+
+}
+
+function change_image(){
+  var image = document.getElementById('image')
+  image.src = "IFaOUvY.jpg"
+}
+
+function change_image(){
+  var image = document.getElementById('image2')
+  image.src = "Family-Quotes-1.jpg"
+}
+
+function increasesize(){
+  var image = document.getElementById("image")
+  image.style.width = "500px"
+}
+
+
+
+
+function decreasesize(){
+  var image = document.getElementById("image")
+  image.style.width = "200px"
+}
